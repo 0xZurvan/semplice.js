e74fa97c86728eec515987111d5c8bec781782bf
0xZurvan <75655454+0xZurvan@users.noreply.github.com>
2023-11-30 17:56:40 -0400

    chore: eslint setup

---
+++ eslint.config.js
@@ -0,0 +1,29 @@
+import antfu from '@antfu/eslint-config'
+import perfectionist from 'eslint-plugin-perfectionist'
+
+export default antfu({
+  stylistic: {
+    indent: 2, // 4, or 'tab'
+    quotes: 'single', // or 'double'
+  },
+
+  // TypeScript and Vue are auto-detected, you can also explicitly enable them:
+  typescript: true,
+
+  // Disable jsonc and yaml support
+  jsonc: false,
+  yaml: false,
+
+  // `.eslintignore` is no longer supported in Flat config, use `ignores` instead
+  ignores: [
+    './fixtures',
+    // ...globs
+  ],
+
+  plugins: {
+    perfectionist,
+  },
+  rules: {
+    'perfectionist/sort-interfaces': 'error',
+  },
+})

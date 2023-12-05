88aacd358e69936e780f43415db7ba053bcdbd08
0xZurvan <75655454+0xZurvan@users.noreply.github.com>
2023-12-05 11:28:21 -0400

    chore: vite library mode and package.json configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,28 @@
 import { URL, fileURLToPath } from 'node:url'
+import { resolve } from 'node:path'
 import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    lib: {
+      name: 'vite-dapp-plugin',
+      entry: [
+        resolve(__dirname, 'src/core/contracts/contract.ts'),
+        resolve(__dirname, 'src/core/address/address.ts'),
+      ],
+      fileName: (format: string, entryName: string) => {
+        if (format === 'es')
+          return `${entryName}.ts`
+
+        return `${entryName}.${format}`
+      },
+    },
+  },
   plugins: [],
   resolve: {
     alias: [
       { find: '@', replacement: fileURLToPath(new URL('./src', import.meta.url)) },
-      { find: '@abi-example', replacement: fileURLToPath(new URL('./src/abi-example', import.meta.url)) },
-      { find: '@abi-example', replacement: fileURLToPath(new URL('./src/abi-example', import.meta.url)) },
-      { find: '@contracts', replacement: fileURLToPath(new URL('./src/core/contracts', import.meta.url)) },
-      { find: '@address', replacement: fileURLToPath(new URL('./src/core/address', import.meta.url)) },
     ],
   },
 })

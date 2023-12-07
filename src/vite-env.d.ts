35cb3f654f081ebec1ec2b9344ec9a0af417f491
0xZurvan <75655454+0xZurvan@users.noreply.github.com>
2023-12-07 16:25:58 -0400

    chore: lib [DOM] option, vite config update, package.json build script update

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -4,6 +4,6 @@ import type { MetaMaskInpageProvider } from '@metamask/providers'
 
 declare global {
   interface Window {
-    ethereum?: MetaMaskInpageProvider
+    ethereum?: any
   }
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,19 @@
 import { URL, fileURLToPath } from 'node:url'
-
-// import path from 'node:path'
+import path from 'node:path'
 import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  /* build: {
+  build: {
     lib: {
       name: 'vite-dapp-plugin',
       entry: [
-        path.resolve(__dirname, 'src/core/contracts/contract.ts'),
-        path.resolve(__dirname, 'src/core/address/address.ts'),
+        path.resolve(__dirname, 'src/core/contract.ts'),
+        path.resolve(__dirname, 'src/core/address.ts'),
+        path.resolve(__dirname, 'src/core/provider.ts'),
       ],
-      fileName: (format: string, entryName: string) => {
-        if (format === 'es')
-          return `${entryName}.ts`
-
-        return `${entryName}.${format}`
-      },
     },
-  }, */
+  },
   plugins: [],
   resolve: {
     alias: [

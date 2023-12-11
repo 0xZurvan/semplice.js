5ec086303dc2b671a788c41d9320f3d77b476801
0xZurvan <75655454+0xZurvan@users.noreply.github.com>
2023-12-11 19:14:06 -0400

    refactor: only browser provider supported

--- src/core/runner.ts
+++ src/core/runner.ts
@@ -1,13 +1,7 @@
-import { BrowserProvider, JsonRpcProvider } from 'ethers'
+import { BrowserProvider } from 'ethers'
 import type { ProviderInstance } from '../types/types'
 
-export function defineProvider(endpoint?: string): ProviderInstance {
-  let provider
-  if (endpoint)
-    provider = new JsonRpcProvider(endpoint)
-  else
-    provider = new BrowserProvider(window.ethereum)
-
+export function defineProvider(): ProviderInstance {
+  const provider = new BrowserProvider(window.ethereum)
   return provider
 }
-
